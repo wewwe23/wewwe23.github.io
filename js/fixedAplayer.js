a04b370a27edcb2d68f26f0aa2a679d5e7d8c60a
wewwe23 <2016925686@qq.com>
2022-04-23 01:00:21 +0800

    Site updated: 2022-04-23 01:00:21

--- js/fixedAplayer.js
+++ js/fixedAplayer.js
@@ -1,21 +1,21 @@
 /*
  * @Author: your name
  * @Date: 2022-04-23 00:41:23
- * @LastEditTime: 2022-04-23 00:42:02
- * @LastEditors: your name
+ * @LastEditTime: 2022-04-23 00:58:18
+ * @LastEditors: Please set LastEditors
  * @Description: ��koroFileHeader�鿴���� ��������: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \undefinedd:\hexo\themes\next\source\js\fixedAplayer.js
  */
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
         {
-            name: '����������',
-            artist: '����',
-            url: '�����ļ��ĵ�ַ',
-            cover: '����ͼƬ�ĵ�ַ',
+            name: 'The Way I Still Love You',
+            artist: 'Reynard Silva',
+            url: 'https://music.163.com/song/media/outer/url?id=28718313.mp3',
+            cover: 'https://p2.music.126.net/JyPsd_g00M-4mqXLLtHncw==/5984641790343690.jpg?',
         }
     ]
 });
