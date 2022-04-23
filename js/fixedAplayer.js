3cb76f80a988423b556e9227abd3986b826b11ae
wewwe23 <2016925686@qq.com>
2022-04-23 14:06:35 +0800

    Site updated: 2022-04-23 14:06:34

--- js/fixedAplayer.js
+++ js/fixedAplayer.js
@@ -1,9 +1,9 @@
 /*
  * @Author: your name
  * @Date: 2022-04-23 00:41:23
- * @LastEditTime: 2022-04-23 13:21:18
+ * @LastEditTime: 2022-04-23 14:05:32
  * @LastEditors: Please set LastEditors
- * @Description: ��koroFileHeader�鿴���� ��������: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \undefinedd:\hexo\themes\next\source\js\fixedAplayer.js
  */
 const ap = new APlayer({
@@ -32,7 +32,7 @@ const ap = new APlayer({
             cover: 'https://p2.music.126.net/u78AmmcaZCO3sKbafA_Gpw==/109951166200230737.jpg?',
         },
         {
-            name: 'Departures ~���ʤ��ˤ����륢���θ�~',
+            name: 'Departures ~あなたにおくるアイの歌~',
             artist: 'EGOIST',
             url: 'https://music.163.com/song/media/outer/url?id=31649312.mp3',
             cover: 'https://p1.music.126.net/6_U3BqyOyORzgT-Pk8EaJA==/7823025232375393.jpg?',
