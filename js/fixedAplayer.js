987ed441560f65fbea9fe1c8e0eb534a89655a72
wewwe23 <2016925686@qq.com>
2022-04-23 13:29:21 +0800

    Site updated: 2022-04-23 13:29:20

--- js/fixedAplayer.js
+++ js/fixedAplayer.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-23 00:41:23
- * @LastEditTime: 2022-04-23 11:07:48
+ * @LastEditTime: 2022-04-23 13:21:18
  * @LastEditors: Please set LastEditors
  * @Description: ��koroFileHeader�鿴���� ��������: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \undefinedd:\hexo\themes\next\source\js\fixedAplayer.js
@@ -26,10 +26,16 @@ const ap = new APlayer({
             cover: 'https://p2.music.126.net/JbyZBW6Th0hbW8RsRXWVdA==/109951166197895701.jpg?',
         },
         {
-            name: 'See You Again',
-            artist: 'Wiz Khalifa / Charlie Puth',
-            url: 'https://music.163.com/song/media/outer/url?id=30953009.mp3',
-            cover: 'https://p2.music.126.net/AuE9HT1fBoRvT23II8Brzw==/109951166715465943.jpg?',
+            name: 'My Dearest',
+            artist: 'supercell',
+            url: 'https://music.163.com/song/media/outer/url?id=825343.mp3',
+            cover: 'https://p2.music.126.net/u78AmmcaZCO3sKbafA_Gpw==/109951166200230737.jpg?',
+        },
+        {
+            name: 'Departures ~���ʤ��ˤ����륢���θ�~',
+            artist: 'EGOIST',
+            url: 'https://music.163.com/song/media/outer/url?id=31649312.mp3',
+            cover: 'https://p1.music.126.net/6_U3BqyOyORzgT-Pk8EaJA==/7823025232375393.jpg?',
         }
     ]
 });
