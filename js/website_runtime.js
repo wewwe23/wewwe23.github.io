ae9d75fbdff8fca490c89e879334faeec838d2f2
wewwe23 <2016925686@qq.com>
2022-04-23 11:55:40 +0800

    Site updated: 2022-04-23 11:55:40

--- js/website_runtime.js
+++ js/website_runtime.js
@@ -1,7 +1,14 @@
-function show_date_time()
-{
+/*
+ * @Author: your name
+ * @Date: 2022-04-23 11:48:43
+ * @LastEditTime: 2022-04-23 11:54:04
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \undefinedd:\hexo\themes\next\source\js\website_runtime.js
+ */
+function show_date_time() {
     window.setTimeout("show_date_time()", 1000);
-    BirthDay = new Date("07/01/2019 00:00:00");
+    BirthDay = new Date("03/26/2022 00:00:00");
     today = new Date();
     timeold = (today.getTime() - BirthDay.getTime());
     sectimeold = timeold / 1000
@@ -18,10 +25,8 @@ function show_date_time()
         .getElementById('days')
         .innerHTML = "已运行 " + daysold + " 天 " + hrsold + " 小时 " + minsold + " 分 " + seconds + " 秒";
 }
-function setzero(i) 
-{
-    if (i < 10) 
-    {
+function setzero(i) {
+    if (i < 10) {
         i = "0" + i
     };
     return i;
