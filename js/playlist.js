cf6ed6f8009ffc5a3e237173ea00c563f7c31eca
wewwe23 <2016925686@qq.com>
2022-04-24 16:43:29 +0800

    Site updated: 2022-04-24 16:43:29

---
+++ js/playlist.js
@@ -0,0 +1,24 @@
+<!--
+ * @Author: your name
+ * @Date: 2022-04-24 16:31:23
+ * @LastEditTime: 2022-04-24 16:35:53
+ * @LastEditors: Please set LastEditors
+ * @Description: ��koroFileHeader�鿴���� ��������: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \undefinedd:\hexo\themes\next\layout\playlist.njk
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/Aplayer.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/Aplayer.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/meting@1.2/dist/Meting.min.js"></script>
+</head>
+
+<body>
+    <meting-js id="4879799554" server="netease" type="playlist" loop="all" autoplay="false"></meting-js>
+</body>
+
+</html>
