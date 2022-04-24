834ca1c5559cc81da12143fde5fb9b1346e7e1f3
wewwe23 <2016925686@qq.com>
2022-04-24 21:36:29 +0800

    Site updated: 2022-04-24 21:36:28

--- js/fixedAplayer.js
+++ js/fixedAplayer.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-04-23 00:41:23
- * @LastEditTime: 2022-04-24 15:34:33
+ * @LastEditTime: 2022-04-24 21:34:47
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \undefinedd:\hexo\themes\next\source\js\fixedAplayer.js
@@ -36,6 +36,24 @@ const ap = new APlayer({
             artist: 'EGOIST',
             url: 'https://music.163.com/song/media/outer/url?id=31649312.mp3',
             cover: 'https://p1.music.126.net/6_U3BqyOyORzgT-Pk8EaJA==/7823025232375393.jpg?',
+        },
+        {
+            name: 'That Girl',
+            artist: 'Olly Murs',
+            url: 'https://music.163.com/song/media/outer/url?id=440208476.mp3',
+            cover: 'https://p2.music.126.net/20i3iOypDSK_4hEKmYj2mg==/109951165975751486.jpg??',
+        },
+        {
+            name: 'You Are Not Alone',
+            artist: 'Michael Jackson',
+            url: 'https://music.163.com/song/media/outer/url?id=1697653.mp3',
+            cover: 'https://p2.music.126.net/4fK_UU_c6IHbq7wrAW4Qow==/18715886929967779.jpg?',
+        },
+        {
+            name: 'Long Long ago, 20th Century',
+            artist: '坂井紀雄',
+            url: 'https://music.163.com/song/media/outer/url?id=26137030.mp3',
+            cover: 'https://p2.music.126.net/owiCkbm4sr7qoZLmvDlcjw==/2368348046254622.jpg?',
         }
     ]
 });
