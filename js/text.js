157e7976fdc0595eb3dc9716efdcb25dff969ea5
wewwe23 <2016925686@qq.com>
2022-06-03 22:44:34 +0800

    Site updated: 2022-06-03 22:44:33

--- js/text.js
+++ js/text.js
@@ -2,15 +2,14 @@
  * @Author: wewwe23 2016925686@qq.com
  * @Date: 2022-06-03 21:48:03
  * @LastEditors: wewwe23 2016925686@qq.com
- * @LastEditTime: 2022-06-03 21:49:30
+ * @LastEditTime: 2022-06-03 22:39:59
  * @FilePath: \undefinedd:\hexo\themes\next\source\js\click_show_text.js.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 var a_idx = 0;
 jQuery(document).ready(function ($) {
     $("body").click(function (e) {
-        var a = new Array
-            ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var a = new Array("樱花", "满地", "集于", "我心", "楪舞", "纷飞", "祈愿", "相随");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
@@ -21,7 +20,7 @@ jQuery(document).ready(function ($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+            "color": "#FF0000"
         });
         $("body").append($i);
         $i.animate({
@@ -39,3 +38,5 @@ jQuery(document).ready(function ($) {
 function delay() {
     $(".buryit").removeAttr("onclick");
 }
+
+
